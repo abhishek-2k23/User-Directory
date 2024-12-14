8d6366380763b1ad27d1ea4a933cfbba0d11d7c1
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-12-14 23:59:24 +0530

    user profile icon

---
+++ components/src/UserDetails/UserProfileIcon.tsx
@@ -0,0 +1,28 @@
+import { View, Text, StyleSheet, useColorScheme } from 'react-native'
+import React from 'react'
+import { userInitials } from '@/utils/Interfaces/UserProfileIcon'
+import { Colors } from '@/constants/Colors'
+import { ThemedText } from '@/components/ThemedText'
+
+const UserProfileIcon = () => {
+    const theme = useColorScheme() || 'light';
+  return (
+    <View style={[styles.container, {borderColor: Colors[theme].text}]}>
+      <ThemedText>AK</ThemedText>
+    </View>
+  )
+}
+
+export default UserProfileIcon
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    width: 30,
+    height: 30, 
+    borderWidth: 1,
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
