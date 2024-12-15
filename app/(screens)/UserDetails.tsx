b4553332a5e22ff70b69ca200c6b04fe7157e721
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-12-15 23:36:04 +0530

    individual user screen component

---
+++ app/(screens)/UserDetails.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native"
+import React from "react"
+import { SafeAreaView } from "react-native-safe-area-context"
+import { ThemedView } from "@/components/ThemedView"
+import { ThemedText } from "@/components/ThemedText"
+
+const UserDetails = () => {
+  return (
+      <ThemedView>
+        <ThemedText>UserDetails</ThemedText>
+      </ThemedView>
+  )
+}
+
+export default UserDetails
