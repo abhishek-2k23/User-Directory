80c7d594e7223a098e9dbe460c59a55f4886ae5a
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-12-15 23:36:27 +0530

    Home screen

---
+++ app/(screens)/index.tsx
@@ -0,0 +1,81 @@
+import { View, Text, StyleSheet, TextInput, useColorScheme, TouchableOpacity } from "react-native"
+import React, { useEffect, useState } from "react"
+import { ThemedView } from "@/components/ThemedView"
+import { ThemedText } from "@/components/ThemedText"
+import { SafeAreaView } from "react-native-safe-area-context"
+import { Link } from "expo-router"
+import { Colors } from "@/constants/Colors"
+import { Ionicons } from "@expo/vector-icons"
+import UserComponent from "@/components/src/index/UserComponent"
+import { fetchUser } from "@/api/fetchUser"
+import useFetchUser from "@/hooks/useFetchUser"
+import UsersComponentList from "@/components/src/index/UsersComponentList"
+
+const Home = () => {
+  const theme = useColorScheme() || "light";
+  const [call, setCall] = useState(true);
+  useFetchUser();
+  return (
+    <SafeAreaView
+      style={[styles.container, { backgroundColor: Colors[theme].background }]}
+    >
+      <ThemedView>
+        {/* app name  */}
+        <ThemedText style={styles.appTitle}> User Directory</ThemedText>
+        {/* search box  & filter option icon */}
+        <ThemedView
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 10,
+          }}
+        >
+          <TextInput
+            style={[
+              styles.textInput,
+              { borderColor: Colors[theme].text, color: Colors[theme].text },
+            ]}
+          ></TextInput>
+          <TouchableOpacity>
+            <ThemedView style={[styles.searchIconBox, {borderColor: Colors[theme].text, }]}>
+              
+              <Ionicons name="search" size={20} color={Colors[theme].text} />
+            </ThemedView>
+          </TouchableOpacity>
+        </ThemedView>
+        {/* user components  */}
+        <ThemedView>
+          <Link href={"/UserDetails"}>
+            <UsersComponentList />
+          </Link>
+        </ThemedView>
+      </ThemedView>
+    </SafeAreaView>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    flex: 1,
+  },
+  appTitle: {
+    fontSize: 24,
+  },
+  textInput: {
+    height: 35,
+    flex: 1,
+    borderWidth: 1,
+    marginVertical: 10,
+    borderRadius: 6,
+  },
+  searchBox: {},
+  searchIconBox: {
+    borderWidth: 1,
+    borderRadius:5,
+    padding: 5,
+  }
+})
