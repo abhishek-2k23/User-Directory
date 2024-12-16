9232997bb39d89f435033804488093c33edd0991
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-12-17 00:48:51 +0530

    remove name chars and add photos

--- components/src/UserDetails/UserProfileIcon.tsx
+++ components/src/UserDetails/UserProfileIcon.tsx
@@ -1,14 +1,13 @@
-import { View, Text, StyleSheet, useColorScheme } from 'react-native'
-import React from 'react'
-import { userInitials } from '@/utils/Interfaces/UserProfileIcon'
+import { View, Text, StyleSheet, useColorScheme, Image } from 'react-native'
+import React, { Profiler } from 'react'
 import { Colors } from '@/constants/Colors'
 import { ThemedText } from '@/components/ThemedText'
 
-const UserProfileIcon = ({firstLetter, secondLetter}: {firstLetter: string, secondLetter: string}) => {
+const UserProfileIcon = () => {
     const theme = useColorScheme() || 'light';
   return (
     <View style={[styles.container, {borderColor: Colors[theme].text}]}>
-      <ThemedText>{firstLetter+secondLetter}</ThemedText>
+      <Image style={styles.profilePic} source={{uri:'https://random-image-pepebigotes.vercel.app/api/random-image'}} />
     </View>
   )
 }
@@ -18,11 +17,16 @@ export default UserProfileIcon
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
-    width: 30,
-    height: 30, 
+    width: 40,
+    height: 40, 
     borderWidth: 1,
     borderRadius: 50,
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  profilePic: {
+    width: 40,
+    height: 40,
+    borderRadius: 100,
   }
 })
