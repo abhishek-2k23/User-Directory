96a6a9b00c5a5a818164bbda9a23982f1fb46728
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-12-17 00:54:21 +0530

    using touchable opacity and passing the userID

--- components/src/index/UserComponent.tsx
+++ components/src/index/UserComponent.tsx
@@ -1,32 +1,49 @@
-import { View, Text, StyleSheet, useColorScheme } from "react-native"
+import {
+  View,
+  Text,
+  StyleSheet,
+  useColorScheme,
+  TouchableOpacity,
+} from "react-native"
 import React, { useEffect } from "react"
 import { ThemedText } from "@/components/ThemedText"
 import { ThemedView } from "@/components/ThemedView"
 import { Colors } from "@/constants/Colors"
 import UserProfileIcon from "../UserDetails/UserProfileIcon"
 import { useDispatch, useSelector } from "react-redux"
-import { getShortInfo } from "@/redux/slice/userSlice"
 import store from "@/redux/store"
 
-const UserComponent = ({name, email}: {name: string, email: string}) => {
-  const theme = useColorScheme() || 'light';
-    
+const UserComponent = ({
+  name,
+  email,
+  onPress,
+}: {
+  name: string
+  email: string
+  onPress: () => void
+}) => {
+  const theme = useColorScheme() || "light"
 
+  
   return (
-    <ThemedView style={[styles.container, {borderColor: Colors[theme].text}]}>
-      {/* left section  */}
-      <ThemedView>
-        {/* name */}
-        <ThemedText> {name}</ThemedText>
-        {/* email  */}
-        <ThemedText style={{paddingLeft: 10,}}> {email}</ThemedText>
-      </ThemedView>
+    <TouchableOpacity onPress={onPress}>
+      <ThemedView
+        style={[styles.container, { borderColor: Colors[theme].text }]}
+      >
+        {/* left section  */}
+        <ThemedView>
+          {/* name */}
+          <ThemedText> {name}</ThemedText>
+          {/* email  */}
+          <ThemedText style={{ paddingLeft: 10 }}> {email}</ThemedText>
+        </ThemedView>
 
-      {/* right section  */}
-      <ThemedView> 
-        <UserProfileIcon firstLetter={name.charAt(0)} secondLetter={name.split(" ")[1].charAt(0)}/>
+        {/* right section  */}
+        <ThemedView>
+          <UserProfileIcon />
+        </ThemedView>
       </ThemedView>
-    </ThemedView>
+    </TouchableOpacity>
   )
 }
 
@@ -37,11 +54,10 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     flex: 1,
     flexDirection: "row",
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    width: '100%',
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
     borderRadius: 10,
-    paddingVertical: 5,
-    paddingHorizontal: 10,
+    padding: 5,
   },
 })
