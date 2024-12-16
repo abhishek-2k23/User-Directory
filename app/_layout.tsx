83cd989f70f785dbebb80d50b1423ac1761182f2
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-12-17 01:00:01 +0530

    stack based app layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,44 +1,39 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native"
+import { useFonts } from "expo-font"
+import { Stack } from "expo-router"
+import * as SplashScreen from "expo-splash-screen"
+import { StatusBar } from "expo-status-bar"
+import "react-native-reanimated"
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { Provider } from 'react-redux';
-import store from '@/redux/store';
+import { useColorScheme } from "@/hooks/useColorScheme"
+import { SafeAreaView } from "react-native-safe-area-context"
+import { Provider } from "react-redux"
+import store from "@/redux/store"
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+  const colorScheme = useColorScheme()
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Provider store={store}>
-      <Stack screenOptions={{headerShown: true}}>
-        <Stack.Screen name="(screens)/index" options={{headerShown: false, headerTitle:'home'}} />
-        <Stack.Screen name='(screens)/UserDetails' options={{headerTitle: 'User Details'}}/>
-      </Stack>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(screens)/index" />
+          <Stack.Screen
+            name="(screens)/UserDetails"
+            options={{ headerTitle: "User Details" }}
+            getId={() => String(Date.now())}
+          />
+        </Stack>
       </Provider>
       <StatusBar style="auto" />
     </ThemeProvider>
-  );
+  )
 }
