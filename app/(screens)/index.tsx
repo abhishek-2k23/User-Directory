4da05d8edd15ec2250b42ec89a9f4404ff4fc663
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-12-18 19:37:39 +0530

    conditional screen for searchedUserDetails

--- app/(screens)/index.tsx
+++ app/(screens)/index.tsx
@@ -1,29 +1,40 @@
-import {
-  StyleSheet,
-  TextInput,
-  useColorScheme,
-  TouchableOpacity,
-} from "react-native"
-import React, { useEffect, useState } from "react"
+import { FlatList, Pressable, StyleSheet, useColorScheme } from "react-native"
+import React, { useEffect } from "react"
 import { ThemedView } from "@/components/ThemedView"
 import { ThemedText } from "@/components/ThemedText"
 import { SafeAreaView } from "react-native-safe-area-context"
-import { Link } from "expo-router"
 import { Colors } from "@/constants/Colors"
-import { Ionicons } from "@expo/vector-icons"
-import UserComponent from "@/components/src/index/UserComponent"
-import { fetchUser } from "@/api/fetchUser"
 import useFetchUser from "@/hooks/useFetchUser"
 import UsersComponentList from "@/components/src/index/UsersComponentList"
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import Loader from "@/components/src/Loader"
 import Search from "@/components/src/index/Search"
+import SearchedUserDetails from "@/components/src/searchedUserDetails"
+import { setSearchScreen } from "@/redux/slice/searchSlice"
+import UserComponent from "@/components/src/index/UserComponent"
+import { router } from "expo-router"
+import { clearSearchResult } from "@/redux/slice/userSlice"
 
 const Home = () => {
-  const theme = useColorScheme() || "light"
-  const loader = useSelector((store: any) => store.users.loader)
-  const fetchAndStoreUsers = useFetchUser()
 
+  const handleUserPress = (userId: string) => {
+      router.push({
+        pathname: "/UserDetails",
+        params: { id: userId },
+      });
+    };
+
+    const handleClose = () => {
+      dispatch(setSearchScreen(false))
+      dispatch(clearSearchResult())
+    }
+  const theme = useColorScheme() || "light"
+  const dispatch = useDispatch()
+  const loader = useSelector((store: any) => store.users.loader)
+  const searchScreen = useSelector((store: any) => store.search.searchScreen) // Fetch searchScreen state
+  const searchResult = useSelector((store: any) => store.users.searchResult) // Fetch search results
+  const searchText = useSelector((store: any) => store.search.searchText) // Fetch the searched text
+  const fetchAndStoreUsers = useFetchUser()
   useEffect(() => {
     fetchAndStoreUsers(true)
   }, [])
@@ -35,18 +46,32 @@ const Home = () => {
       <ThemedView>
         {/* app name  */}
         <ThemedText style={styles.appTitle}> User Directory</ThemedText>
-        
+
         {/* search box  & filter option icon */}
         <Search />
-        
+
         {/* user components  */}
+        {/* Conditional Rendering */}
         {loader ? (
           <ThemedView style={styles.loaderContainer}>
             <Loader />
           </ThemedView>
+        ) : searchScreen ? (
+          searchResult?.length > 0 ? (
+            <SearchedUserDetails searchResult={searchResult} searchText={searchText} />
+          ) : (
+            <ThemedView style={{flexDirection: 'row', width: '95%', alignItems: 'center', justifyContent: 'space-between'}}>
+              {" "}
+              <ThemedText>No Users Exist</ThemedText>{" "}
+              <Pressable onPress={() => dispatch(setSearchScreen(false))}>
+                <ThemedText>X</ThemedText>
+              </Pressable>
+            </ThemedView>
+          )
         ) : (
-            <UsersComponentList />
+          <UsersComponentList />
         )}
+
       </ThemedView>
     </SafeAreaView>
   )
