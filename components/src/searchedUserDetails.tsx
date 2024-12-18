257a6304c0bb76e9abb95ae329bd37788de18851
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-12-18 19:37:15 +0530

    searchedUserDetails screen

---
+++ components/src/searchedUserDetails.tsx
@@ -0,0 +1,90 @@
+import React from "react"
+import { FlatList, Pressable, StyleSheet, View } from "react-native"
+import { ThemedText } from "@/components/ThemedText"
+import UserComponent from "@/components/src/index/UserComponent"
+import { ThemedView } from "../ThemedView"
+import { useDispatch } from "react-redux"
+import { setSearchScreen } from "@/redux/slice/searchSlice"
+import { router } from "expo-router"
+import Loader from "./Loader"
+import { clearSearchResult } from "@/redux/slice/userSlice"
+
+const SearchedUserDetails = ({
+  searchText,
+  searchResult,
+}: {
+  searchText: string
+  searchResult: { id: string; name: string; email: string }[] | null // Updated type to allow null
+}) => {
+  console.log(searchText, searchResult)
+  const dispatch = useDispatch()
+
+  const handleClose = () => {
+    dispatch(setSearchScreen(false)) // Close the search screen
+    dispatch(clearSearchResult());
+  }
+
+  const handleUserPress = (userId: string) => {
+    router.push({
+      pathname: "/UserDetails",
+      params: { id: userId },
+    })
+  }
+
+  // If searchResult is null, show loader
+  if (searchResult === null) {
+    return <Loader />
+  }
+
+  return (
+    <ThemedView>
+      {/* Header */}
+      <ThemedView
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          width: '98%'
+        }}
+      >
+        <ThemedText style={{ marginBottom: 10 }}>
+          You searched for: {searchText}
+        </ThemedText>
+
+        <Pressable onPress={handleClose}>
+          <ThemedText>X</ThemedText>
+        </Pressable>
+      </ThemedView>
+      <FlatList
+        data={searchResult}
+        renderItem={({ item, index }) => (
+          <UserComponent
+            name={item?.name}
+            email={item?.email}
+            itemNo={index + 1}
+            onPress={() => handleUserPress(item?.id)}
+          />
+        )}
+        keyExtractor={(item) => item.id}
+        contentContainerStyle={{ gap: 16 }}
+        ListEmptyComponent={
+          <ThemedText>No users found for "{searchText}"</ThemedText>
+        }
+      />
+    </ThemedView>
+  )
+}
+
+export default SearchedUserDetails
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+  },
+  header: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+})
