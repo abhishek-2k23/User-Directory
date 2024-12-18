93328b7997f622075358d555207523c5d63bbef4
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-12-18 19:36:28 +0530

    add search reducer

--- redux/store.ts
+++ redux/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from './slice/userSlice'
+import searchSlice from "./slice/searchSlice";
 const store = configureStore({
     reducer: {
         users: userSlice,
+        search: searchSlice,
     }
 })
 
